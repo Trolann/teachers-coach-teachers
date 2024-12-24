ea89ffe31c7e72e9dc6fc05c415922865cff59e1
Trevor Mathisen <mathisentc@gmail.com>
2024-12-24 14:36:30 -0800

    model and migration creations

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
-import os
-import sys
+from os import path as os_path
+from sys import path as sys_path
 # Add flask_app to path
-flask_app_path = os.path.join(os.path.dirname(__file__), 'flask_app')
-sys.path.insert(0, flask_app_path)
+flask_app_path = os_path.join(os_path.dirname(__file__), 'flask_app')
+sys_path.insert(0, flask_app_path)
 
 # Now import using the exact path where create_app exists
 from flask_app.app import create_app
