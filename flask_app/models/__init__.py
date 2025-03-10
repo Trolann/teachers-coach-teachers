56840ecec1e3a6c6e77d09f88f2d3f214a1ba8d7
Trevor Mathisen <mathisentc@gmail.com>
2025-03-10 11:58:42 -0700

    refactor: Remove unused models from import and __all__

--- flask_app/models/__init__.py
+++ flask_app/models/__init__.py
@@ -2,15 +2,13 @@ from extensions.database import db
 from extensions.logging import get_logger
 
 # Import all models here to register them with SQLAlchemy
-from flask_app.models.user import User, MyTable
-from flask_app.models.mentor_profiles import MentorProfile, MentorStatus
+from flask_app.models.user import User
+from flask_app.models.mentor_profiles import MentorStatus
 from flask_app.models.mentorship_session import MentorshipSession
 from flask_app.models.credits import CreditRedemption, CreditTransfer
 
 __all__ = [
     'User',
-    'MyTable', 
-    'MentorProfile',
     'MentorStatus',
     'MentorshipSession',
     'CreditRedemption',
