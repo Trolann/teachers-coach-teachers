8326ae0bbedc9b8c53761aff35f9a984dc1dbf43
dyashik <yashikdhanaraj@gmail.com>
2025-05-16 15:06:04 -0700

    new credentials

--- mobile/constants/StreamDemoCredentials.ts
+++ mobile/constants/StreamDemoCredentials.ts
@@ -1,7 +1,7 @@
 export const STREAM_DEMO_CREDS = {
     apiKey: 'mmhfdzb5evj2',
     token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0dyZWVkbyIsInVzZXJfaWQiOiJHcmVlZG8iLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTc0NjYyNDM0NywiZXhwIjoxNzQ3MjI5MTQ3fQ.rQHL3GDpiJ1uRZ6NDhipUcHeK2newVfUCHLR2hCSX9M',
-    userId: 'Greedo',
-    callId: 'cAUvibGnuYnU',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0x1a2VfU2t5d2Fsa2VyIiwidXNlcl9pZCI6Ikx1a2VfU2t5d2Fsa2VyIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3NDc0MDAzMzcsImV4cCI6MTc0ODAwNTEzN30.hotQvskMbT5tU2mwy2I2av5mRYOMEmH-AaXBg8HpOvE',
+    userId: 'Luke_Skywalker',
+    callId: 'kpPXRQgDQ4uy',
 } as const;
